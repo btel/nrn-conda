a95572fb3350c13965df981451b78c0a5acc7616
Bartosz Telenczuk <muchatel@poczta.fm>
2014-05-12 15:28:19 +0200

    move libs

--- build.sh
+++ build.sh
@@ -3,5 +3,9 @@
 ./configure --with-nrnpython=$PYTHON --prefix=$PREFIX --without-iv
 make 
 make install
+
+mv $PREFIX/x86_64/bin/* $PREFIX/bin
+mv $PREFIX/x86_64/lib/* $PREFIX/lib
+
 cd src/nrnpython
-$PYTHON setup.py install
+$PYTHON setup.py install --prefix=$PREFIX
