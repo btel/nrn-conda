a26c3baf5b1a4fb83550fa5fdf538a37a08a0bb5
Bartosz <muchatel@poczta.fm>
2014-10-22 02:47:10 +0200

    fixed lib paths

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-./configure --with-nrnpython=$PYTHON --prefix=$PREFIX --without-iv
+./configure --with-nrnpython=$PYTHON --prefix=$PREFIX --exec-prefix=$PREFIX --without-iv PYLIBLINK='-framework Python' PYLIB='-framework Python'
 make 
 make install
 
-mv $PREFIX/x86_64/bin/* $PREFIX/bin
-mv $PREFIX/x86_64/lib/* $PREFIX/lib
+#mv $PREFIX/x86_64/bin/* $PREFIX/bin
+#mv $PREFIX/x86_64/lib/* $PREFIX/lib
 
 cd src/nrnpython
-$PYTHON setup.py install --prefix=$PREFIX
+$PYTHON setup.py install --prefix=$PREFIX 
